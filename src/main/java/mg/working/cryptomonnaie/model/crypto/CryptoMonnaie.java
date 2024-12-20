a3b19869a87a3e6fd17df7e4a588c762b8b47e69
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-12-20 14:56:27 +0300

    Create Cryptomonnai

---
+++ src/main/java/mg/working/cryptomonnaie/model/crypto/CryptoMonnaie.java
@@ -0,0 +1,45 @@
+package mg.working.cryptomonnaie.model.crypto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "crypto_monnaie")
+@SequenceGenerator(
+    name = "s_crypto_monnaie",
+    sequenceName = "s_crypto_monnaie",
+    allocationSize = 1
+)
+public class CryptoMonnaie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "s_crypto_monnaie")
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "designation", nullable = false, length = 100)
+    private String designation;
+
+    @Column(name = "symbol", nullable = false, unique = true, length = 10)
+    private String symbol;
+
+    @Column(name = "prix_unitaire", nullable = false, precision = 15, scale = 2)
+    private double prixUnitaire;
+
+    public void setPrixUnitaire(String prixUnitaire) {
+        this.prixUnitaire = Double.parseDouble(prixUnitaire);
+    }
+}
+
