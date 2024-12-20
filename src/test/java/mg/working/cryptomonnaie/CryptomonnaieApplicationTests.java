e4679a1a77a369b497418eea1757e9067106eeca
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-12-20 13:42:46 +0300

    Initial commit

---
+++ src/test/java/mg/working/cryptomonnaie/CryptomonnaieApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.working.cryptomonnaie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptomonnaieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
