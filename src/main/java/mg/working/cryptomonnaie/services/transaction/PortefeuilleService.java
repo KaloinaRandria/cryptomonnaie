2a8fb34768c58e56bed7d17c6b641659956bce79
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-01-05 19:51:14 +0300

    update portefeuille quantite apres vente

--- src/main/java/mg/working/cryptomonnaie/services/transaction/PortefeuilleService.java
+++ src/main/java/mg/working/cryptomonnaie/services/transaction/PortefeuilleService.java
@@ -6,6 +6,7 @@ import mg.working.cryptomonnaie.repository.transaction.PortefeuilleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -23,5 +24,10 @@ public class PortefeuilleService {
 
     public Portefeuille getPortefeuilleByUser(Utilisateur utilisateur) {
         return this.portefeuilleRepository.findByUser(utilisateur);
+    }
+
+    public void updateQuantiteCrypto(Portefeuille portefeuille , BigDecimal quantiteAVendre) {
+        portefeuille.setQuantite(portefeuille.getQuantite().subtract(quantiteAVendre));
+        this.insertPortefeuille(portefeuille);
     }
 }
