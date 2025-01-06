e928d31d026f4780738433b313182862a6a17faa
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2025-01-06 23:42:46 +0300

    Initiation Portefeuille Utilisateur

---
+++ src/main/java/mg/working/cryptomonnaie/controller/transaction/PortefeuilleController.java
@@ -0,0 +1,10 @@
+package mg.working.cryptomonnaie.controller.transaction;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/portefeuille")
+public class PortefeuilleController {
+    
+}
